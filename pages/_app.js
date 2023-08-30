dec75370b4a82543f60f9543944f8e7782811897
Ahmad-Alanati <ahmadjehadalanati1@gmail.com>
2023-08-30 09:58:46 +0300

    done some work

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import Footer from './Footer';
-import '../styles/globals.css'; 
+import '@/styles/globals.css'
+import { AuthProvider } from '@/contexts/auth'
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="flex-grow">
-        <Component {...pageProps} />
-      </div>
-      {/* <Footer /> */}
-    </div>
-  );
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,15 @@ import ABooking from "@/components/AppointmentForm";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { user, login, token } = useAuth()
+
+  function loginFormHundeler(event) {
+    event.preventDefault();
+    const username = event.target.username.value
+    const password = event.target.password.value
+    console.log(username, password)
+    login(username, password)
+  }
   return (
     <div>
 
