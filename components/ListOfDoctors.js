3fe7b251362509a5e98ec3bf71558f2a02cc6463
leenalzaben <leena.zaben61@gmail.com>
2023-09-05 10:03:10 +0000

    edit the list of doctors

--- components/ListOfDoctors.js
+++ components/ListOfDoctors.js
@@ -1,56 +1,40 @@
 import Image from 'next/image';
 import { doctors } from '@/data';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import SwiperCore, { Navigation } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import { useState } from 'react';
-
-SwiperCore.use([Navigation]);
 
 export default function Home() {
   const [hoveredCard, setHoveredCard] = useState(null);
 
   return (
     <div className="container mx-auto p-4">
-      <div className="mt-4">
-        <Swiper
-          spaceBetween={30}
-          slidesPerView={6}
-          navigation
-          loop={true}
-          onReachEnd={(swiper) => swiper.slideTo(0)}
-          className="swiper-container"
-        >
-          {doctors && Array.isArray(doctors) && doctors.length > 0 ? (
-            doctors.map((doctor, index) => (
-              <SwiperSlide key={doctor.id} className="swiper-slide">
-                <div
-                  className="doctor-card bg-sky-950 text-white rounded-2xl overflow-hidden shadow-inner transition-transform transform hover:scale-105"
-                  onMouseEnter={() => setHoveredCard(index)}
-                  onMouseLeave={() => setHoveredCard(null)}
-                >
-                  <div className="relative w-full h-56">
-                    <Image
-                      src={doctor.avatar}
-                      alt={`Dr. ${doctor.name}`}
-                      layout="fill"
-                      objectFit="cover"
-                    />
-                  </div>
-                  <div className="p-4">
-                    <h2 className="text-xl font-mono">{doctor.name}</h2>
-                    {hoveredCard === index && (
-                      <p className="text-sm font-mono">{doctor.additionalInfo}</p>
-                    )}
-                  </div>
-                </div>
-              </SwiperSlide>
-            ))
-          ) : (
-            <p>No doctors available</p>
-          )}
-        </Swiper>
+      <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+        {doctors && Array.isArray(doctors) && doctors.length > 0 ? (
+          doctors.map((doctor, index) => (
+            <div
+              key={doctor.id}
+              className="doctor-card bg-sky-950 text-white rounded-2xl overflow-hidden shadow-inner transition-transform transform hover:scale-105"
+              onMouseEnter={() => setHoveredCard(index)}
+              onMouseLeave={() => setHoveredCard(null)}
+            >
+              <div className="relative w-full h-56">
+                <Image
+                  src={doctor.avatar}
+                  alt={`Dr. ${doctor.name}`}
+                  layout="fill"
+                  objectFit="cover"
+                />
+              </div>
+              <div className="p-4">
+                <h2 className="text-xl font-mono">{doctor.name}</h2>
+                {hoveredCard === index && (
+                  <p className="text-sm font-mono">{doctor.additionalInfo}</p>
+                )}
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No doctors available</p>
+        )}
       </div>
     </div>
   );
